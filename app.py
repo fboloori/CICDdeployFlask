06ae7c278aa6614c079fc4d289c88232b9ae2ba2
Fatemeh <f.boloori@gmail.com>
2025-01-03 06:03:10 -0800

    heart attack project

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify ,render_template , redirect
 import pickle
 import numpy as np
+from model import MLmodel
 
 app = Flask(__name__)
+mlmodel = MLmodel()
 
-with open(r"model.pkl", "rb") as input_file:
-    model = pickle.load(input_file)
 
-@app.route("/predict", methods=["POST"])
+@app.route('/')
+def home():
+	return render_template('main.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    data = request.get_json()
-    features = np.array(data["features"]).reshape(1, -1)
-    prediction = model.predict(features)[0]
-    return jsonify({"prediction" : prediction})
+    if request.method == 'POST':
+        if request.is_json:
+            # Handle JSON input
+            print("handle json")
+            data = request.get_json()
+            features = np.array(data["features"]).reshape(1, -1)
+            prediction = mlmodel.predict(features)[0]
+            return jsonify({"prediction": prediction})
+
+        else:
+            # Handle form data input 
+            age = int(request.form['age'])
+            sex = request.form.get('sex')
+            cp = int(request.form.get('cp'))  # Assuming it's an integer
+            trestbps = int(request.form['trestbps'])
+            chol = int(request.form['chol'])
+            fbs = int(request.form['fbs'])  # Assuming it's an integer
+            restecg = int(request.form['restecg'])
+            thalach = int(request.form['thalach'])
+            exang = int(request.form['exang'])  # Assuming it's an integer
+            oldpeak = float(request.form['oldpeak'])
+            slope = int(request.form['slope'])  # Assuming it's an integer
+            ca = int(request.form['ca'])
+            thal = int(request.form['thal'])  # Assuming it's an integer
+
+            data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]] , dtype=float)
+            my_prediction = mlmodel.predict(data)[0]
+            print(data , my_prediction)
+            return render_template('result.html', prediction=my_prediction)
       
 if __name__=="__main__":
     app.run(debug = True, host="0.0.0.0", port=5000 )      
--- model.py
+++ model.py
@@ -1,29 +1,50 @@
+# importing required libraries 
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier 
+
+class MLmodel:
+
+    def __init__(self):
+            self.scaler= StandardScaler()
+            heart = pd.read_csv("heart_cleveland_upload.csv")
+            heart_df = heart.copy()
+            heart_df = heart_df.rename(columns={'condition':'target'})
+
+            x= heart_df.drop(columns= 'target')
+            y= heart_df.target
+            x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=42)
+
+            x_train_scaler= self.scaler.fit_transform(x_train)
+            x_test_scaler= self.scaler.fit_transform(x_test)
+
+            self.model=RandomForestClassifier(n_estimators=20)
+            self.model.fit(x_train_scaler, y_train)
+
+            y_pred= self.model.predict(x_test_scaler)
+            self.score = self.model.score(x_test_scaler,y_test)
+            self.cm = confusion_matrix(y_test, y_pred)
+
+            print(self.score)
+            print('Classification Report\n', classification_report(y_test, y_pred))
+            print('Accuracy: {}%\n'.format(round((accuracy_score(y_test, y_pred)*100),2)))
+            print(self.cm)
+
+            # Creating a pickle file for the classifier
+            self.modelfilename = 'model.pkl'
+            pickle.dump(self.model, open(self.modelfilename, 'wb'))
+            print('pickle file saved. ')   
+
+    def predict(self , newx):
+            newx_scaler= self.scaler.fit_transform(newx)
+            with open(r"model.pkl", "rb") as input_file:
+                model = pickle.load(input_file)
+            res = model.predict(newx)
+            print(res)
+            return (res)
 
 
-
-def train_and_save_model():
-    #print(pickle.format_version)
-    data = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv')
-    X = data.drop(columns=["medv"])
-    y = data['medv']
-
-
-    X_Train, X_Test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = LinearRegression()
-    model.fit(X_Train, y_train)
-
-    with open("model.pkl", "wb") as file:
-        pickle.dump(model, file)
-
-
-    print('pickle file saved. ')   
-
-if __name__  == "__main__":
-    train_and_save_model()     
-
-     
 
